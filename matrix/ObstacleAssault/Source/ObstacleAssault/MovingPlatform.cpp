b99027ec0c516596a69e635d0b191ed28576035f
LordSahvith <bulletproofchild@gmail.com>
2024-03-16 15:10:00 -0600

    using tick function

--- matrix/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ matrix/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -14,8 +14,6 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SetActorLocation(MyVector);
 }
 
 // Called every frame
@@ -23,5 +21,9 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MyVector.X = MyVector.X + 1;
+	MyVector.Y = MyVector.Y + 1;
+
+	SetActorLocation(MyVector);
 }
 
