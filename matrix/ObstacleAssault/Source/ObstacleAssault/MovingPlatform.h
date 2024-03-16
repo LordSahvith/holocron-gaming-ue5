e4296e43406e3d86a5cb998ab5ce92441eda6951
LordSahvith <bulletproofchild@gmail.com>
2024-03-16 17:42:03 -0600

    BP Child class from C++ class

--- matrix/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ matrix/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -24,7 +24,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
-	FVector PlatformVelocity = FVector(1, 0, 0);
+	FVector PlatformVelocity = FVector(100, 0, 0);
 
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	float MoveDistance = 100;
