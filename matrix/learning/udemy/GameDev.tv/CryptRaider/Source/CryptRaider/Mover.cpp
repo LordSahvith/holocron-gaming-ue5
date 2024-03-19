7920cd9717d944d18bb21df7c1f1ccaa28d3258b
LordSahvith <bulletproofchild@gmail.com>
2024-03-18 19:23:19 -0600

    Dereferencing & Arrow Operator (->)

--- matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/Mover.cpp
+++ matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/Mover.cpp
@@ -28,8 +28,15 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	AActor* Owner = GetOwner();
+	float MyFloat = 5;
+	float* YourFloat = &MyFloat;
+	UE_LOG(LogTemp, Display, TEXT("My Float: %f"), *YourFloat);
 
-	UE_LOG(LogTemp, Warning, TEXT("Mover Owner Address: %u"), &Owner);
+	AActor* Owner = GetOwner();
+	FString OwnerName = Owner->GetActorNameOrLabel(); // using a struct FString or FVector use (*Owner).GetActorNameOrLabel();
+	UE_LOG(LogTemp, Display, TEXT("%s....I am your Tick!!!"), *OwnerName);
+
+	FVector OwnerLocation = Owner->GetActorLocation();
+	UE_LOG(LogTemp, Display, TEXT("%s. Your location is: %s"), *OwnerName, *OwnerLocation.ToCompactString());
 }
 
