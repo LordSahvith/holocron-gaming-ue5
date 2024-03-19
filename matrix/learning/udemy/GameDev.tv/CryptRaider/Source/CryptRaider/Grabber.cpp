8bb7a79366eb6be40b5bb5e5ab99eb847ed82db3
LordSahvith <bulletproofchild@gmail.com>
2024-03-19 17:04:46 -0600

    FindComponentByClass() & nullptr

--- matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/Grabber.cpp
+++ matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/Grabber.cpp
@@ -4,6 +4,7 @@
 
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -20,7 +21,16 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UPhysicsHandleComponent *PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle != nullptr)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Got Physics Handle: %s"), *PhysicsHandle->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NO PHYSICS HANDLE EQUIPPED"));
+	}
 }
 
 // Called every frame
