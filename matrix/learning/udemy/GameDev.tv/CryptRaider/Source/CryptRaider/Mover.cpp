5bf65f8ee9c8863bd0df3d05981db53cda1c6d84
LordSahvith <bulletproofchild@gmail.com>
2024-03-18 18:59:05 -0600

    create actor component; mover

---
+++ matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/Mover.cpp
@@ -0,0 +1,34 @@
+// Dev Lord Savith : Course: GameDev.tv
+
+#include "Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Flea....I am your tick!!"));
+}
+
