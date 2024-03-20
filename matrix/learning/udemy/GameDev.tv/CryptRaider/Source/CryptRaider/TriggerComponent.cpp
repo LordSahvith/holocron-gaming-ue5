97d712bf1c91cbe3f84c7d0beca3ef83b3c83d0d
LordSahvith <bulletproofchild@gmail.com>
2024-03-19 20:45:36 -0600

    Constructor

--- matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/TriggerComponent.cpp
+++ matrix/learning/udemy/GameDev.tv/CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -5,11 +5,7 @@
 // Sets default values for this component's properties
 UTriggerComponent::UTriggerComponent()
 {
-    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-    // off to improve performance if you don't need them.
     PrimaryComponentTick.bCanEverTick = true;
-
-    // ...
 }
 
 // Called when the game starts
@@ -17,4 +13,9 @@ void UTriggerComponent::BeginPlay()
 {
     Super::BeginPlay();
     UE_LOG(LogTemp, Display, TEXT("Trigger Component"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
