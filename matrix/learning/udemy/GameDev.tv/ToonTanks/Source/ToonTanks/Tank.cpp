19c5e98fdc3173c48269e5ce6638f1d9487ff87f
LordSahvith <bulletproofchild@gmail.com>
2024-03-20 22:37:59 -0600

    Local rotation and move tank

--- matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/Tank.cpp
+++ matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/Tank.cpp
@@ -23,11 +23,19 @@ void ATank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
     Super::SetupPlayerInputComponent(PlayerInputComponent);
 
     PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+    PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
 }
 
 void ATank::Move(float Value)
 {
     FVector DeltaLocation = FVector::ZeroVector;
     DeltaLocation.X = Speed * Value * UGameplayStatics::GetWorldDeltaSeconds(this);
-    AddActorLocalOffset(DeltaLocation);
+    AddActorLocalOffset(DeltaLocation, true);
+}
+
+void ATank::Turn(float Value)
+{
+    FRotator DeltaRotation = FRotator::ZeroRotator;
+    DeltaRotation.Yaw = TurnRate * Value * UGameplayStatics::GetWorldDeltaSeconds(this);
+    AddActorLocalRotation(DeltaRotation, true);
 }
