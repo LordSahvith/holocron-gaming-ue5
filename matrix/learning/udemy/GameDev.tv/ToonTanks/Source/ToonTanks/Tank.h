c59ae3768d35d1253d09e568513b135180b5c372
LordSahvith <bulletproofchild@gmail.com>
2024-03-20 22:11:40 -0600

    add speed to tank using delta time

--- matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/Tank.h
+++ matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/Tank.h
@@ -23,5 +23,9 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	class UCameraComponent *Camera;
 
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Speed = 200.f;
+
 	void Move(float Value);
+
 };
