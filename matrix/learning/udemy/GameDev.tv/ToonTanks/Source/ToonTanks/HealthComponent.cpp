a47208b9fec8b8924ee57e7dd80fe2c35fea3bec
LordSahvith <bulletproofchild@gmail.com>
2024-03-22 19:13:02 -0600

    start health component

---
+++ matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/HealthComponent.cpp
@@ -0,0 +1,32 @@
+// DEV: Lord Savith ---- COURSE: GameDev.tv
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::DamageTaken);
+}
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHealthComponent::DamageTaken(AActor *DamagedActor, float Damage, const UDamageType *DamageType, class AController *Instigator, AActor *DamageCauser)
+{
+	
+}
