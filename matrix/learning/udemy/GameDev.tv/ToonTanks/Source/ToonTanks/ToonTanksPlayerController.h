2b4127e277fc230ab851e55e5e3c2629a1ee846f
LordSahvith <bulletproofchild@gmail.com>
2024-03-23 15:42:12 -0600

    create player controller

---
+++ matrix/learning/udemy/GameDev.tv/ToonTanks/Source/ToonTanks/ToonTanksPlayerController.h
@@ -0,0 +1,16 @@
+// DEV: Lord Savith ---- COURSE: GameDev.tv
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ToonTanksPlayerController.generated.h"
+
+UCLASS()
+class TOONTANKS_API AToonTanksPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	void SetPlayerEnabledState(bool bPlayerEnabled);
+};
