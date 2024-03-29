3d4378eafc3c74406336934c59a76915a7d6bad1
LordSahvith <bulletproofchild@gmail.com>
2024-03-29 15:24:21 -0600

    have enemy follow actor

--- matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
+++ matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -17,4 +17,8 @@ class SIMPLESHOOTER_API AShooterAIController : public AAIController
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
