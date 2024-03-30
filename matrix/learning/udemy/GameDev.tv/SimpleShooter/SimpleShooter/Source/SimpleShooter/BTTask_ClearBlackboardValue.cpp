b5ed9c2b3eefcac65c8c16d32b7f19f2a7e2a9a6
LordSahvith <bulletproofchild@gmail.com>
2024-03-29 18:34:54 -0600

    Start working with Behavior Tasks

---
+++ matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,8 @@
+// DEV: Lord Savith -- COURSE: GameDev.tv Udemy
+
+#include "BTTask_ClearBlackboardValue.h"
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
+{
+    NodeName = TEXT("Clear Blackboard Value");
+}
