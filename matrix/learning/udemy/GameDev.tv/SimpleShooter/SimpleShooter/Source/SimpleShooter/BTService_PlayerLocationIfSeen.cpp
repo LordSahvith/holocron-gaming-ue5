5f84f4ca04c0ddcecbde3fbe034eee63030c196f
LordSahvith <bulletproofchild@gmail.com>
2024-03-29 21:41:51 -0600

    Create Services to help with the Behavior Tree

---
+++ matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/BTService_PlayerLocationIfSeen.cpp
@@ -0,0 +1,35 @@
+// DEV: Lord Savith -- COURSE: GameDev.tv Udemy
+
+#include "BTService_PlayerLocationIfSeen.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "kismet/GameplayStatics.h"
+#include "AIController.h"
+
+UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
+{
+    NodeName = TEXT("Update Player Location If Seen");
+}
+
+void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory, float DeltaSeconds)
+{
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+    if (OwnerComp.GetAIOwner() == nullptr)
+        return;
+
+    APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    if (PlayerPawn == nullptr)
+        return;
+
+    if (OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn))
+    {
+        OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());
+    }
+    else
+    {
+        OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+    }
+
+    return;
+}
