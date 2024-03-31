3f7b8c583e514d0e160308b910c1219befb5342a
LordSahvith <bulletproofchild@gmail.com>
2024-03-31 16:32:17 -0600

    show lose screen upon death

--- matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/SimpleShooterPlayerController.cpp
+++ matrix/learning/udemy/GameDev.tv/SimpleShooter/SimpleShooter/Source/SimpleShooter/SimpleShooterPlayerController.cpp
@@ -2,10 +2,17 @@
 
 #include "SimpleShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void ASimpleShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner)
 {
     Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+    UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+    if (LoseScreen)
+    {
+        LoseScreen->AddToViewport();
+    }
     
     GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
